df62fc4d79a3e1d15a2b83f8ae0593f7b285a677
Abdul Samad <samadcom797@gmail.com>
2024-10-11 14:15:38 +0500

    Project initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { React } from 'react';
+
+function App() {
+  return (
+    <p className="bg-primary">
+      
+    </p>
+  );
+}
+
+export {App};
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import { App } from '/src/App.jsx';
+
+import '/src/Styles/index.css';
+import '/src/Styles/reset.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).
+  render(<App />);
