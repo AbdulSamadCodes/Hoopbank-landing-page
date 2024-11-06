390d5dd0eade4b181d68bdc6a37a2f7b50a8f8c5
Abdul Samad <samadcom797@gmail.com>
2024-11-06 12:46:08 +0500

    Working on features section

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,6 +1,5 @@
 import { React } from 'react';
 import { Hero } from '/src/Components/Hero.jsx'
-import { Stats } from '/src/Components/Stats.jsx'
 import { Features } from '/src/Components/Features.jsx'
 
 
@@ -10,7 +9,6 @@ function Container() {
   return (
     <div className={`${styles.layout["container"]}`}>
       <Hero />
-      <Stats />
       <Features />
     </div>
   );
